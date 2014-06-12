e012e7d52aa397dee3b86acd81f4631f00c546b2
snoweye <wccsnow@gmail.com>
2014-06-12 19:03:40 -0400

    update

--- install_cran_pbdR.r
+++ install_cran_pbdR.r
@@ -3,9 +3,9 @@
 # install <- paste(github.dir, "install_dp_pbdR.r", sep = "")
 # eval(parse(text = getURL(install, ssl.verifypeer = FALSE)))
 
-dropbox.dir <- "http://dl.dropboxusercontent.com/u/86659524/"
+cran.dir <- "http://cran.r-project.org/bin/windows/contrib/3.1/"
 win.bin <- c(
-  "pbdMPI_0.2-5.zip",
+  "pbdMPI_0.2-4.zip",
   "pbdSLAP_0.1-9.zip",
   "SEXPtools_0.1-0.zip",
   "pbdBASE_0.3-0.zip",
@@ -15,7 +15,7 @@ win.bin <- c(
 
 tmp.dir <- tempdir()
 for(i.win.bin in win.bin){
-  bin.file <- paste(dropbox.dir, i.win.bin, sep = "")
+  bin.file <- paste(cran.dir, i.win.bin, sep = "")
   tmp.file <- paste(tmp.dir, i.win.bin, sep = "/")
   download.file(bin.file, tmp.file)
   install.packages(tmp.file, repos = NULL)
