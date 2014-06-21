68a3a34689eab643ee2c2ec1a14ece4fa07ccde3
snoweye <wccsnow@gmail.com>
2014-06-21 13:39:09 -0400

    update pbdNCDF4 version

--- install_dp_pbdR.r
+++ install_dp_pbdR.r
@@ -10,7 +10,7 @@ win.bin <- c(
   "SEXPtools_0.1-0.zip",
   "pbdBASE_0.3-0.zip",
   "pbdDMAT_0.3-0.zip",
-  "pbdNCDF4_0.1-3.zip",
+  "pbdNCDF4_0.1-4.zip",
   "pbdDEMO_0.3-0.zip",
   "pbdPROF_0.2-4.zip",
   "pmclust_0.1-7.zip"
