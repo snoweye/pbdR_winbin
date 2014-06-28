6c5ab216076b4f9273adcde4d86974fa48e800f8
snoweye <wccsnow@gmail.com>
2014-06-28 13:57:25 -0400

    updates

--- install_cran_pbdR.r
+++ install_cran_pbdR.r
@@ -7,10 +7,13 @@ cran.dir <- "http://cran.r-project.org/bin/windows/contrib/3.1/"
 win.bin <- c(
   "pbdMPI_0.2-4.zip",
   "pbdSLAP_0.1-9.zip",
-  "SEXPtools_0.1-0.zip",
+  "RNACI_0.1.0.zip",
   "pbdBASE_0.3-0.zip",
   "pbdDMAT_0.3-0.zip",
-  "pbdDEMO_0.3-0.zip"
+  "pbdNCDF4_0.1-4.zip",
+  "pbdDEMO_0.3-0.zip",
+  "pbdPROF_0.2-4.zip",
+  "pmclust_0.1-7.zip"
 )
 
 tmp.dir <- tempdir()
